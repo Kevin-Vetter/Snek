e8daa791d9810a43e2f58244d8333bfa0ccd0b2c
Kevin Vetter <kevinmvetter@gmail.com>
2022-09-08 14:48:14 +0200

    Did Canvas pattern

--- Snek/MainWindow.xaml.cs
+++ Snek/MainWindow.xaml.cs
@@ -24,5 +24,48 @@ namespace Snek
         {
             InitializeComponent();
         }
+
+        const int SquareSize = 20;
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            DrawArena();
+        }
+        private void DrawArena()
+        {
+            bool backgroundDrawn = false;
+            int nextX = 0, nextY = 0;
+            int rowCounter = 0;
+            bool nextIsOdd = false;
+
+            while (!backgroundDrawn)
+            {
+                Rectangle rectangle = new()
+                {
+                    Width = SquareSize,
+                    Height = SquareSize,
+                    Fill = nextIsOdd ? Brushes.YellowGreen : Brushes.OliveDrab
+                };
+
+                Arena.Children.Add(rectangle);
+                Canvas.SetTop(rectangle, nextY);
+                Canvas.SetLeft(rectangle, nextX);
+
+                nextIsOdd = !nextIsOdd;
+                nextX += SquareSize;
+                if (nextX >= Arena.ActualWidth)
+                {
+                    nextX = 0;
+                    nextY += SquareSize;
+                    rowCounter++;
+                    nextIsOdd = rowCounter % 2 != 0;
+                }
+
+                if (nextY >= Arena.ActualHeight)
+                {
+                    backgroundDrawn = true;
+                }
+            }
+        }
     }
 }
